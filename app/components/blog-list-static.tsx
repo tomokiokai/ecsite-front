3bbb80a7c13894453a5040dca202f7167d2d5e89
tomoki <tomokiokai0715@gmail.com>
2024-01-05 16:02:45 +0900

    blogs_getSpecificCookies_allCookiesログ確認

--- app/components/blog-list-static.tsx
+++ app/components/blog-list-static.tsx
@@ -8,7 +8,7 @@ import { redirect } from 'next/navigation';
 export const getSpecificCookies = (): { token: string | null, csrfToken: string | null } => {
   const cookieStore = cookies();
   const allCookies = cookieStore.getAll();
-
+  console.log("allCookies:", allCookies);
   // 特定のクッキーの値を取得
   const token = allCookies.find(cookie => cookie.name === 'token')?.value || null;
   const csrfToken = allCookies.find(cookie => cookie.name === '_csrf')?.value || null;
